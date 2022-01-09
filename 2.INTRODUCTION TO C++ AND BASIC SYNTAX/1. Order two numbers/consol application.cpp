04597e49d6b7f0911a2f7a96ff655427a8e1fd7e
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-09 21:44:51 +0200

    Add files via upload

---
+++ 2.INTRODUCTION TO C++ AND BASIC SYNTAX/1. Order two numbers/consol application.cpp
@@ -0,0 +1,23 @@
+﻿#include<string>
+#include<vector>
+#include<cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+
+	cin >> a >> b;
+	if (a < b) {
+
+		cout << a << " "<< b << endl;
+	}
+	else {
+		cout << b <<" "<< a << endl;
+	}
+	
+}
+
---
+++ 2.INTRODUCTION TO C++ AND BASIC SYNTAX/2. Product Sign/Product Sign/Product Sign.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	int product = a * b * c;
+	if (product >= 0) {
+		cout << "+" << endl;
+	}
+	else
+	{
+		cout << "-" << endl;
+	}
+}
+
