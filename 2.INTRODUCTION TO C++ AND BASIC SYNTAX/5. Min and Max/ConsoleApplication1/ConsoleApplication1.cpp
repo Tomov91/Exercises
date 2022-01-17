2d4e821952256dbecce6e1a82a242338a8c75c96
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-17 12:08:01 +0200

    Add files via upload

---
+++ 2.INTRODUCTION TO C++ AND BASIC SYNTAX/5. Min and Max/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include<algorithm>
+#include<vector>
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+	std::vector<int> nums;
+	while (n--)
+	{
+		int a;
+		std::cin >> a;
+		nums.push_back(a);
+	}
+		std::cout << *min_element(nums.begin(), nums.end()) << " "<< *max_element(nums.begin(), nums.end());
+		
+	
+}
+
