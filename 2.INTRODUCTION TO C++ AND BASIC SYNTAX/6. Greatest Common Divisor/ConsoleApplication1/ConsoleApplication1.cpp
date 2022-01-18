2beeaac6f2e4fd9391f52be7471e847b569d91c8
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-18 08:36:26 +0200

    Add files via upload

---
+++ 2.INTRODUCTION TO C++ AND BASIC SYNTAX/6. Greatest Common Divisor/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, n2, numb1, numb2, temp, lcm, gcd;
+    cin >> n1 >> n2;
+
+    numb1 = n1;
+    numb2 = n2;
+
+    while (numb2 != 0) {
+        temp = numb2;
+        numb2 = numb1 % numb2;
+        numb1 = temp;
+
+    }
+    gcd = numb1;
+    lcm = (n1 * n2) / gcd;
+    cout << gcd;
+}
