04c7eebf5fbe902f52ab899ec846ab4f4bdadd6c
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-19 10:20:31 +0200

    Add files via upload

---
+++ 3. Exercises on Basic Syntax, Functions and Arrays/Problem 1 – Operations/ConsoleApplication1/Problem 1 – Operations/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	double left, right;
+	char sign= 0;
+	int sum;
+	cin >> left >> right;
+	while (!sign)
+	{
+		cin >> sign;
+		switch (sign)
+		{
+		case '+': sum = left + right; break;
+		case '-': sum = left - right; break;
+		case '*': sum = left * right; break;
+		case '/': sum = left / right; break;
+
+		default:
+			sign = 0;
+			cout << "try again";
+		}
+	}
+	cout << sum;
+}
