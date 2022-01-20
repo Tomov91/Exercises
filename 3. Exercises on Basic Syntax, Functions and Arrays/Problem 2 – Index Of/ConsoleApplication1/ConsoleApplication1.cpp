3a74bec0b000f10ca3cf8c9ce452c25c45c1ca91
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-20 09:11:14 +0200

    Add files via upload

---
+++ 3. Exercises on Basic Syntax, Functions and Arrays/Problem 2 – Index Of/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include<vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int theChosenOne;
+	vector<int> nums;
+	while (n--)
+	{
+		int m;
+		cin >> m;
+		nums.push_back(m);
+	}
+	cin >> theChosenOne;
+	cout << nums[theChosenOne];
+}
+
