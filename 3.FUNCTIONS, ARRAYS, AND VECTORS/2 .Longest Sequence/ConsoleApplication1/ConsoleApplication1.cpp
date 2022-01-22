d767dbf0fc4eb4075f97934cad641f3abc9cb969
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-22 11:45:23 +0200

    Add files via upload

---
+++ 3.FUNCTIONS, ARRAYS, AND VECTORS/2 .Longest Sequence/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,47 @@
+﻿#include<vector>
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+
+	int n;
+	int currentMatch = 1;
+	int maxMatch = 1;
+	int store = 0;
+	int counter = 0;
+	cin >> n;
+	vector<int> sequence;
+
+	while (n--)
+	{
+		int m;
+		cin >> m;
+		sequence.push_back(m);
+	}
+	for (size_t i = 1; i < sequence.size(); i++)
+	{
+		if (sequence[i] == sequence[i - 1]) {
+			currentMatch++;
+			if (currentMatch > maxMatch) {
+				store = sequence[i];
+				maxMatch = currentMatch;
+			}
+		}
+		else
+		{
+			currentMatch = 1;
+		}
+
+	}
+	if (maxMatch == 1) {
+		int s = sequence.size();
+		cout << sequence[s - 1];
+	}
+	else {
+		for (size_t i = 0; i < maxMatch; i++)
+		{
+			cout << store << " ";
+		}
+	}
+}
