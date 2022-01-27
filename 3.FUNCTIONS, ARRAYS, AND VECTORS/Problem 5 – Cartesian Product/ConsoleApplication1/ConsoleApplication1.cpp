d4aa0019268d175dc2a45c841e2aa364b99a493b
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-27 14:51:16 +0200

    Add files via upload

---
+++ 3.FUNCTIONS, ARRAYS, AND VECTORS/Problem 5 – Cartesian Product/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include<vector>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int m;
+    int sum;
+    vector<int> nums;
+    cin >> n;
+    while (n--)
+    {
+        cin >> m;
+        nums.push_back(m);
+    }
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+        for (size_t j = 0; j < nums.size(); j++)
+        {
+            sum = nums[i] * nums[j];
+            cout << sum << " ";
+        }
+    }
+}
+
