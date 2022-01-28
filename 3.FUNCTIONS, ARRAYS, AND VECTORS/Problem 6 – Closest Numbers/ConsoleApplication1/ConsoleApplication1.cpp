8cb07ee4a3b4bd8a2f264bcf31fc8dab50eae85b
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-28 11:47:49 +0200

    Add files via upload

---
+++ 3.FUNCTIONS, ARRAYS, AND VECTORS/Problem 6 – Closest Numbers/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include<vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int m;
+	vector<int> nums;
+	int sum = 0;
+	int prevSum = 0;
+	int store = 1000;
+	cin >> n;
+	while (n--)
+	{
+		cin >> m;
+		nums.push_back(m);
+	}
+	for (size_t i = 0; i < nums.size(); i++)
+	{
+		
+		for (size_t j = i+1; j < nums.size(); j++)
+		{
+			
+			
+			sum = abs(nums[i] - nums[j]);
+			if (sum < store) {
+				store = sum;
+			}
+				prevSum = sum;
+		}
+		
+	}
+	cout << store;
+}
+
+
