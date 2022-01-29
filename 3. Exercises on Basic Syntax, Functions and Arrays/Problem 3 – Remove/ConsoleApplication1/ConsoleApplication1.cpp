3529f5e3dd65668452a7d43838676e6e064d743f
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-29 11:59:49 +0200

    Add files via upload

---
+++ 3. Exercises on Basic Syntax, Functions and Arrays/Problem 3 – Remove/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include<vector>
+#include <iostream>
+using namespace std;
+int main()
+{
+    vector<int> numbers;
+    vector<int>::iterator new_numbers;
+    int n;
+    cin >> n;
+    int theChosenOne;
+    int i = 0;
+    while (n--)
+    {
+        int m;
+        cin >> m;
+        numbers.push_back(m);
+    }
+    cin >> theChosenOne;
+    for (size_t i = 0; i < numbers.size(); i++)
+    {
+        if (numbers[i] != theChosenOne) {
+            cout << numbers[i] << " ";
+
+        }
+       // new_numbers = remove(numbers.begin(), numbers.end(), theChosenOne);
+        //cout << numbers[i] << " ";
+    }
+}
+
