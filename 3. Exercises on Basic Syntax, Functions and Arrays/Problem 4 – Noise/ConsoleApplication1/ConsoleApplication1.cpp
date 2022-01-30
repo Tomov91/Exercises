c86512c6f8f8bdd0d64f89921722bd31a05a808a
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-30 10:55:48 +0200

    Add files via upload

---
+++ 3. Exercises on Basic Syntax, Functions and Arrays/Problem 4 – Noise/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include<vector>
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string input;
+	string number;
+	cin >> input;
+	for (const char& c : input) {
+		if (isdigit(c)) {
+			number += c;
+		}
+	}
+	int result = stoi(number);
+	cout << sqrt(result);
+}
