ad7ed96768b4e14afc9d4d1aef36d58dca461550
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-01-31 10:48:45 +0200

    Add files via upload

---
+++ 3. Exercises on Basic Syntax, Functions and Arrays/Problem 5 – Noise Test Generator/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include<vector>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+
+int ReadIntWithNoise(string str) {
+	unsigned index = 0;
+	int number = 0;
+
+	for (index = 0; index < str.length(); index++) {
+		char symbol = str[index];
+		if (symbol >= '0' && symbol <= '9') {
+			number = number * 10;  // up number with ^10
+			number = number + (symbol - '0');  // add the new number 
+		}
+		else
+			if (symbol == '.')
+				break;
+	}
+
+	return number;
+}
+
+int main()
+{
+	// initialize pseudorandom generator
+	srand((unsigned)time(NULL));
+
+	// get the size of the random sequence
+	int count = 100; // ((unsigned)rand()) % 100 + 1; // count ==> 1 .. 65
+
+	string str;
+	for (; count; count--)
+		str.push_back((char)(rand()));
+
+	cout << "Generated sequence: " << endl;
+	cout << str << endl;
+	cout << "=============" << endl;
+
+	int result = ReadIntWithNoise(str);
+	cout << "Our number is: " << result << endl;
+	cout << "sqrt(result) = " << sqrt(result) << endl;
+}
