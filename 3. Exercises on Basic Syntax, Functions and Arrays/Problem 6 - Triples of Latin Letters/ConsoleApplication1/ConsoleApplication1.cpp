dc7212b5eb8e75b37f39799c770b12fa8ffe3c1d
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-02 11:27:35 +0200

    Add files via upload

---
+++ 3. Exercises on Basic Syntax, Functions and Arrays/Problem 6 - Triples of Latin Letters/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include<vector>
+#include <iostream>
+#include<string>
+using namespace std;
+
+/*Problem 6 - Triples of Latin Letters
+Write a program to read an integer n and print all triples of the first n small Latin letters, ordered
+alphabetically:*/
+
+int main()
+{
+	int n;
+	cin >> n;
+	char letter = 'a';
+	int b = letter;
+	for (size_t i = 0; i < n; i++)
+		for (size_t j = 0; j < n; j++)
+			for (size_t k = 0; k < n; k++)
+				cout << (char)(i + 'a') << (char)(j + 'a') << (char)(k + 'a') << endl;
+
+	/*int d = 1;
+	cout << (char)(d + 'a');*/
+}
---
+++ 3. Exercises on Basic Syntax, Functions and Arrays/Problem 7 – Print Name of Number/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,68 @@
+﻿/*Write a program that, given an integer number in the range [0, 9999], prints the name of that number in English.
+Simplifications:
+ Use lowercase English letters only
+ Don’t place “and” (e.g. 957 is nine hundred fifty seven, NOT nine hundred and fifty seven)
+ Skip 0 digits, except for the number 0 (e.g. 0 -&gt; zero; 101 -&gt; one hundred one; 1001 -&gt; one thousand
+one)
+ Don’t print dashes (e.g. print 75 as seventy five, NOT seventy-five)*/
+#include <iostream>
+
+using namespace std;
+
+string DigitToWord(int digit) {
+
+	switch (digit)
+	{
+
+	case 2: return "two"; break;
+	case 3: return "three"; break;
+	case 4: return "four"; break;
+	case 5: return "five"; break;
+	case 6: return "six"; break;
+	case 7: return "seven"; break;
+	case 8: return "eight"; break;
+	case 9: return "nine"; break;
+	default:
+		return ""; break;
+	}
+
+}
+
+
+int main()
+{
+    int number;
+    cin >> number;
+	if (number == 0) {
+		cout << "zero" << endl;
+	}
+	if (number > 999) {
+		cout << "Number should be less than 1000" << endl;
+	}
+
+	int d1, d2, d3, d4; // d1 d2 d3 d4
+						//  1  2  4  5 ==> 1245
+
+	d1 = number / 1000;		// => d1
+	d2 = (number % 1000) / 100; // => d2
+	d3 = (number % 100) / 10;	// => d3
+	d4 = number % 10;			// => d4
+	if (d1)
+		cout << DigitToWord(d1) << " thousand ";
+	if (d2)
+		cout << DigitToWord(d2) << " hundred ";
+	if (d3)
+		switch (d3) {
+		case 1: cout << "ten "; break;
+		case 2: cout << "twenty "; break;
+		case 3: cout << "thirty "; break;
+		case 4: cout << "fourty "; break;
+		case 5: cout << "fifty "; break;
+		case 6: cout << "sixty "; break;
+		case 7: cout << "seventy "; break;
+		case 8: cout << "eighty "; break;
+		case 9: cout << "ninety "; break;
+		}
+	if (d4)
+		cout << DigitToWord(d4);
+}
