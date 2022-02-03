e8468fa402a28c74575f8e50fe1d4b406a921ec2
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-03 10:28:03 +0200

    Add files via upload

---
+++ 3. Exercises on Basic Syntax, Functions and Arrays/Problem 8 – Multiply Evens Sum by Odds Sum/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿//Create a program that reads an integer number and multiplies the sum of all its even digits by the sum of all its
+//odd digits 
+
+#include <iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin >> n;
+    int even=0;
+    int  odd=0;
+    string input = to_string(n);
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        if(input[i]%2 == 0)
+        even += input[i]-48;
+        else
+            odd += input[i]-48;
+        
+    }
+       
+    int result = int(even) * (int)odd;
+    cout << result;
+}
+
