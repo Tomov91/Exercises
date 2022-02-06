7e33119822319f4030e9e8015dad7f1a43c9e2fe
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-06 12:39:18 +0200

    Add files via upload

---
+++ 5.STRINGS AND STREAMS/Problem 2 – Mathematical Expression/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <string>
+#include<sstream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    string nums;
+    getline(cin, nums);
+    int counter = 0;
+    istringstream stream(nums);
+    char n;
+    vector<char> vec;
+
+    while (stream >> n) {
+        vec.push_back(n);
+    }
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] == ')' or vec[i] == '(') {
+            counter++;
+        }
+    }
+    if (counter % 2 == 0) {
+        cout << "correct" << endl;
+    }
+    else
+    {
+        cout << "incorrect" << endl;    
+    }
+}
+
