97298516a128cb9b1e5b52375588554b00afcf6f
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-07 13:51:14 +0200

    Add files via upload

---
+++ 5.STRINGS AND STREAMS/Problem 3 – Title Case/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include<vector>
+#include<string>
+#include<sstream>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    string expresion;
+    getline(cin, expresion);
+
+    for (size_t i = 1; i < expresion.size(); i++)
+    {
+        if (expresion[i-1] == ' ' and expresion[i]>96) {
+            expresion[i] = expresion[i] - 32;
+        }
+    }
+    cout << expresion << endl;
+}
+
