bae29c79d4c99146d648cb7ad59a7da0ce427ae9
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-22 09:55:48 +0200

    Add files via upload

---
+++ 8,STL ASSOCIATIVE CONTAINERS/Problem 1 - Count Real Numbers/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include<vector>
+#include<set>
+#include<list>
+#include <iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string numbers;
+    getline(cin, numbers);
+    stringstream str(numbers);
+    double temp;
+    vector<double> nums;
+    set<double> mySet;
+    while (str>>temp)
+    {
+        nums.push_back(temp);
+        mySet.insert(temp);
+    }
+    
+    for (auto n : mySet) {
+        cout << n << " -> " << count(nums.begin(), nums.end(), n) << endl;
+    }
+    
+    
+}
+
