76e89d1249fb01dd8d1a732158d55e067f234c26
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-24 16:14:44 +0200

    Add files via upload

---
+++ 8,STL ASSOCIATIVE CONTAINERS/Problem 2 – Odd Occurrences/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿#include<string>
+#include <iostream>
+#include<sstream>
+#include<set>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void getOddOccurrence(vector<string>& arr)
+{
+    set<string> mySet;
+    for (int i = 0; i < arr.size(); i++) {
+
+        int count = 0;
+
+        for (int j = 0; j < arr.size(); j++) {
+            if (arr[i] == arr[j])
+                count++;
+        }
+        if (count % 2 != 0)
+            mySet.insert(arr[i]);
+    }
+    
+    for (set<string>::reverse_iterator it = mySet.rbegin(); it != mySet.rend(); it++) {
+        cout << *it << ", ";
+    }
+    
+}
+
+int main()
+{
+    string line;
+    getline(cin, line);
+    stringstream str(line);
+    string word;
+    vector<string> arr;
+    while (str>>word)
+    {
+        std::for_each(word.begin(), word.end(), [](char& c) {
+            c = ::tolower(c);
+            });
+        arr.push_back(word);;
+    }
+    //transform(arr.begin(), arr.end(), arr.begin(), ::tolower);
+    getOddOccurrence(arr);
+}
