5e4e539c98563ee0fed108d0ad97249eba44346f
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-25 13:29:57 +0200

    Add files via upload

---
+++ 8,STL ASSOCIATIVE CONTAINERS/Problem 3 – Largest 3/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include<vector>
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+int main()
+{
+    int n = 3;
+    vector<int> myVec{ 10 ,30 ,15 ,20 ,50, 5 };
+    partial_sort(myVec.begin(), myVec.begin() + 3, myVec.end(), greater<int>());
+    for (size_t i = 0; i < myVec.size(); i++)
+    {
+            cout << myVec[i] << " ";   
+    }
+    
+}
+
