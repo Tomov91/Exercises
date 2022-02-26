1fced8afcefd8c9d3be7df5b11151642626eaccc
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-26 10:07:50 +0200

    Add files via upload

---
+++ 8,STL ASSOCIATIVE CONTAINERS/Problem 4 – Short Words/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include<set>
+#include<string>
+#include <iostream>
+#include<algorithm>
+#include<sstream>
+using namespace std;
+/*Read a text, extract its words (separated by spaces) find all short words (less than 5 characters) and print them alphabetically, in lowercase, separate by a single comma and a single space
+•	Use case-insensitive matching.
+•	Remove duplicated words.
+*/
+int main()
+{
+    int count = 0;
+    string line;
+    string tempstr;
+    getline(cin, line);
+    set<string> mySet;
+
+    stringstream str(line);
+    while (str >> tempstr)
+    {
+        transform(tempstr.begin(), tempstr.end(), tempstr.begin(), ::tolower);
+        mySet.insert(tempstr);
+    }
+    for (string s : mySet) {
+        for (char c : s) {
+            count++;
+        }
+            if (count < 5) {
+                cout << s << ", ";
+            }
+        count = 0;
+    }
+}
