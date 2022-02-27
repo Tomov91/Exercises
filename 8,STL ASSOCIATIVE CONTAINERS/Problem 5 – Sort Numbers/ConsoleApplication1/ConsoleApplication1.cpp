a1375cc9a1da97c846576da39498e59f74beeae5
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-27 10:53:30 +0200

    Add files via upload

---
+++ 8,STL ASSOCIATIVE CONTAINERS/Problem 5 – Sort Numbers/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include<string>
+#include<vector>
+#include<algorithm>
+#include <iostream>
+#include<sstream>
+using namespace std;
+/*Read a list of decimal numbers and sort them in increasing order. Print the output as shown in the examples below*/
+int main()
+{
+    int temp;
+    string line;
+    getline(cin, line);
+    stringstream str(line);
+    vector<int> arr;
+    while (str >> temp)
+    {
+        arr.push_back(temp);
+    }
+    sort(arr.begin(), arr.end());
+    for (int n : arr) {
+        cout << n << " ";
+    }
+}
+
