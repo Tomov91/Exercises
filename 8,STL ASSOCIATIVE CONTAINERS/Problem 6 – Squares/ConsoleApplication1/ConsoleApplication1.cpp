bf661afecb6df702500915bc5bfb0261873c5d13
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-02-28 16:04:16 +0200

    Add files via upload

---
+++ 8,STL ASSOCIATIVE CONTAINERS/Problem 6 – Squares/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include<vector>
+#include <iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+using namespace std;
+
+/*Read a list of integers and extract all square numbers from it and print them in descending order. 
+A square number is an integer which is the square of any integer. For example, 1, 4, 9, 16 are square numbers.*/
+
+int main()
+{
+    string line;
+    getline(cin, line);
+    stringstream str(line);
+    int temp;
+    vector<int> arr;
+    int var = 4;
+    while (str>>temp)
+    {
+        arr.push_back(temp);
+    }
+    sort(arr.begin(), arr.end(), greater<int>());
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        int sqr = sqrt(arr[i]);
+        if ((sqr * sqr) == arr[i]) {
+            cout << arr[i] << " ";
+        }
+    }
+   
+}
+
