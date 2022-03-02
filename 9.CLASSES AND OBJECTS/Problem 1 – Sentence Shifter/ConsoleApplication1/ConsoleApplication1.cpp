aa1659e25f08d244fba97ce04a634ff2954feb88
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-03-02 10:16:31 +0200

    Add files via upload

---
+++ 9.CLASSES AND OBJECTS/Problem 1 – Sentence Shifter/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include<string>
+#include <iostream>
+#include<string>
+#include<vector>
+#include<sstream>
+using namespace std;
+
+int main()
+{
+	string line;
+	getline(cin, line);
+	int n;
+	cin >> n;
+	stringstream str(line);
+	vector<string> myVec;
+	vector<string> newVec;
+	string newLine;
+	int count = 0;
+	while (str>>newLine)
+	{
+		myVec.push_back(newLine);
+	}
+	
+	for (size_t i=myVec.size()-n; i < myVec.size(); i++)
+	{
+		newVec.push_back(myVec[i]);
+	}
+	for (size_t i = 0; i < myVec.size()-n; i++)
+	{
+		newVec.push_back(myVec[i]);
+	}
+	for (auto n : newVec) {
+		cout << n << " ";
+	}
+	
+
+}
