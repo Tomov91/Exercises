73d73f7e2fc600682f0ea56796c0bcbfa2b81aa8
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-03-15 16:05:33 +0200

    Add files via upload

---
+++ Exam Preparation/Problem 2 – Sort Array by Order/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <list>
+
+
+using namespace std;
+/*5
+1 2 3 * &
+*/
+int main()
+{
+
+	list<char> charList;
+
+	int countNum;
+	cin >> countNum;
+
+	char c;
+
+	while (countNum--) {
+		cin >> c;
+		charList.push_back(c);
+	}
+
+	string numbers;
+	string letters;
+	string other;
+
+	for (list<char>::iterator it = charList.begin(); it != charList.end(); it++) {
+		if (*it >= '0' && *it <= '9') {
+			//cout << *it << " ";
+			numbers.push_back(*it);
+		}
+		else if (*it >= 'a' && *it <= 'z') {
+			letters.push_back(*it);
+		}
+		else {
+		other.push_back(*it);
+		}
+	}
+
+	cout << numbers << endl;
+	cout << letters << endl;
+	cout << other << endl;
+
+	return 0;
+}
