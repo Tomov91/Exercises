621c2c727663846599cb26d81c080ae99b19b461
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-03-16 10:42:47 +0200

    Add files via upload

---
+++ 5.STRINGS AND STREAMS/Problem 7 – The Noise and the Signal/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include<string>
+#include<vector>
+#include<algorithm>
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+    string line;
+    getline(cin, line);
+	vector<string> myVec;
+	string temp;
+	for (size_t i = 0; i < line.size(); i++)
+	{
+		if (line[i] == '-' or isalpha(line[i])) {
+			temp.push_back(line[i]);	
+		}
+		if (isspace(line[i])) {
+			if(!temp.empty())
+			myVec.push_back(temp);
+			temp = "";
+		}
+		
+	}
+	sort(myVec.begin(), myVec.end());
+		if (!myVec.empty()) {
+			cout << myVec[0];
+		}
+		else
+		{
+			cout << "no noise";
+		}
+}
